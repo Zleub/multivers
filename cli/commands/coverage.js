e94b0687fd30311dc9715afda74a5ea9311837d6
zleub <adebray@student.42.fr>
2018-07-27 19:27:12 +0200

    cli config

--- cli/commands/coverage.js
+++ cli/commands/coverage.js
@@ -2,14 +2,14 @@ const execSync = require('child_process').execSync
 
 const cmd = 'flow-coverage-report'
 const args = [
+  `-c 8`,
   `-i "*.js"`,
   `-i "**/*.js"`,
-  `-x "cli.js"`,
-  `-x "cli/**"`,
   `-x "flow-coverage/**"`,
   `-x "flow-typed/**"`,
   `-x "node_modules/**"`,
-  `-x "server_build/**"`
+  `-x "*_build/**"`,
+  `-x "docs/**"`,
 ]
-module.exports = () =>
-  execSync(`${cmd} ${args.join(' ')}`).toString()
+module.exports = (argv) =>
+  execSync(`${cmd} ${args.join(' ')} ${argv.join(' ')}`).toString()
